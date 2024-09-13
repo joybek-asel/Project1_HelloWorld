6db24862c5a4cc4f35d23b9eb81e1f14ca3308b2
Assel <erikbaevaovechka@gmail.com>
2024-09-13 16:09:17 +0600

    first commit

---
+++ src/test/java/org/example/project1_javaee/Project1JavaeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project1_javaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1JavaeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
